27d04c9ee5ebf2bd3924a99311f31d9ddce9ce3c
jackiecwtsoi <jackiecwtsoi@gmail.com>
2022-06-21 17:07:33 +0800

    Added an output line regarding Jenkins build.

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter{
     public String sayHello() {
-        return "Hello World! This is a simple dockerized Java program built using Gradle~";
+        return "Hello World! This is a simple dockerized Java program built using Gradle~ Now it's being built on Jenkins!";
     }
 }
